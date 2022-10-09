fc2aa444a42b0e540d1c278baf8887f8f2684a6d
Gizem Izmir <gizkes1996@gmail.com>
2022-10-09 20:33:45 +0300

    MessageItem component updated

--- src/components/MessageItem/MessageItem.js
+++ src/components/MessageItem/MessageItem.js
@@ -1,14 +1,13 @@
 import React, { useRef, useEffect } from "react";
 import { View, Text } from "react-native";
 import styles from "./MessageItem.style";
-import Icon from "@expo/vector-icons/Ionicons";
 import MapView, { Marker } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 
 // MessageItem is used to list the message on the Chat Screen
 const MessageItem = ({ message, status, type }) => {
   const mapRef = useRef();
-  const {navigate} = useNavigation();
+  const { navigate } = useNavigation();
 
   useEffect(() => {
     mapRef?.current?.fitToSuppliedMarkers({
@@ -22,11 +21,17 @@ const MessageItem = ({ message, status, type }) => {
         <View style={styles.messageTextContainer}>
           <Text style={styles.messageText}>{message.text}</Text>
         </View>
+        <View style={styles.messageIcon}>
+          <Text style={styles.messageDate}>{message.timestamp}</Text>
+        </View>
       </View>
     ) : (
       <View style={styles.receiveMessage}>
         <View style={styles.messageTextContainer}>
           <Text style={styles.messageText}>{message.text}</Text>
+        </View>
+        <View style={styles.messageIcon}>
+          <Text style={styles.messageDate}>{message.timestamp}</Text>
         </View>
       </View>
     )
@@ -37,7 +42,9 @@ const MessageItem = ({ message, status, type }) => {
       showsScale
       minZoomLevel={5}
       getMarkersFrames={true}
-      onPress={() => navigate("Location", { latitude: message.lat, longitude: message.long})}
+      onPress={() =>
+        navigate("Location", { latitude: message.lat, longitude: message.long })
+      }
     >
       <Marker
         identifier="location"
@@ -51,7 +58,9 @@ const MessageItem = ({ message, status, type }) => {
       showsScale
       minZoomLevel={5}
       getMarkersFrames={true}
-      onPress={() => navigate("Location", { latitude: message.lat, longitude: message.long})}
+      onPress={() =>
+        navigate("Location", { latitude: message.lat, longitude: message.long })
+      }
     >
       <Marker
         identifier="location"
